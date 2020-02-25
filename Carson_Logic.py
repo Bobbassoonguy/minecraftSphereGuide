d32a2e9a29253b575720ca7865a084a031d546fc
Mathcrafter00 <carsonaveryjones@gmail.com>
2020-02-25 14:41:52 -0500

    carson do the commit #2

--- Carson_Logic.py
+++ Carson_Logic.py
@@ -5,9 +5,15 @@ import random
 def circle(radius):
     print(radius)
     r2 = radius**2
+    array = [[]]
+    for x in range(0, 2*radius):
+        for z in range(0, 2*radius):
+            array[x][z] = 0
     for z in range(0, radius):
         z2 = z**2
         dist = r2-z2
+        x2 = dist
+        x = math.sqrt(x2)
 
 
 circle(5)
