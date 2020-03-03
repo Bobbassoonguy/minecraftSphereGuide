8f7a45771dd8ae88bbf3f7c0df14f9b863f82667
Mathcrafter00 <carsonaveryjones@gmail.com>
2020-03-03 17:26:02 -0500

    Carson_Logic actually makes circle-like shapes now

--- Carson_Logic.py
+++ Carson_Logic.py
@@ -2,19 +2,33 @@ import math
 import random
 
 
+def f(x):
+    if(x == -1):
+        return 0
+    return x
+
+
 def circle(radius):
     print(radius)
+    array = [[0 for i in range(2*radius)] for j in range(2*radius)]
     r2 = radius**2
-    array = [[]]
-    for x in range(0, 2*radius):
-        for z in range(0, 2*radius):
-            array[x][z] = 0
-    for z in range(0, radius):
+    for z in range(0, math.ceil(radius)):
         z2 = z**2
         dist = r2-z2
         x2 = dist
         x = math.sqrt(x2)
+        print((radius-0.5)**2)
+        print((radius+0.5)**2)
+        for n in range(math.floor(x)-1, math.floor(x) + 2):
+            print(str(z) + ", " + str(n) + ", " + str(z**2+n**2))
+            if z**2+n**2 > (radius-0.5)**2 and z2 + n**2 <= (radius+0.5)**2:
+                print("yes")
+                for s in range(-1, 2, 2):
+                    for t in range(-1, 2, 2):
+                        array[n*s+math.floor(radius-f(s))][z*t+math.floor(radius-f(t))] = 1
+                        array[z*t+math.floor(radius-f(t))][n*s+math.floor(radius-f(s))] = 1
+    for row in array:
+        print(row)
 
 
-print("Do the thing")
 circle(5)
