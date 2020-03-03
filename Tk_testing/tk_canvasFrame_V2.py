fc0f956f0aff7ec8481388c7f665ffe47d0f53f8
ashaftel <alexthebassoonguy@gmail.com>
2020-03-03 15:17:51 -0500

    better zooming and panning

--- Tk_testing/tk_canvasFrame_V2.py
+++ Tk_testing/tk_canvasFrame_V2.py
@@ -17,20 +17,37 @@ class ResizingCanvas(Canvas):
 
     def on_resize(self, event):
         # determine the ratio of old width/height to new width/height
-        if event.width > event.height:
-            # print("Window wide")
-            scale = float(event.width) / self.width
-            self.width = event.width
-            self.height = self.width
-        else:
-            # print("Window tall")
-            scale = float(event.height) / self.height
-            self.height = event.height
-            self.width = self.height
+        if (round(event.width) != round(self.width) or round(event.height) != round(self.height)) and not self.supressResize:
+            if event.width > event.height:
+                # print("Window wide")
+                scale = float(event.width) / self.width
+                self.width = event.width
+                self.height = self.width
+            else:
+                # print("Window tall")
+                scale = float(event.height) / self.height
+                self.height = event.height
+                self.width = self.height
+            # resize the canvas, update scrollregion
+            self.config(width=self.width, height=self.height, scrollregion=(0, 0, self.width, self.height))
+            # rescale all the objects tagged with the "all" tag
+            self.scale("all", 0, 0, scale, scale)
+
+            print("resized:", self.width, ",", self.height, self.scrollregion)
+        self.supressResize = False
+
+    def zoom(self, scale): # TODO figure out why before window is ever resized, zoom changes window size
+        self.supressResize = True
+
+        # determine the ratio of old width/height to new width/height
+        self.width = round(self.width * scale)
+        self.height = self.width
+
         # resize the canvas, update scrollregion
         self.config(width=self.width, height=self.height, scrollregion=(0, 0, self.width, self.height))
         # rescale all the objects tagged with the "all" tag
         self.scale("all", 0, 0, scale, scale)
+        print("Zoomed:", self.width, ",", self.height, self.scrollregion)
 
     def scroll_start(self, event):
         self.scan_mark(event.x, event.y)
@@ -56,6 +73,9 @@ class SquareDispCanvas:
         self.canvas.grid(row=0, column=0, sticky=NSEW)
         self.frame.columnconfigure(0, weight=1)
         self.frame.rowconfigure(0, weight=1)
+
+        self.canvasScrolling()
+        self.canvasZoomButtons()
 
         self.drawCanvasGrid()
         self.drawCanvasAxes()
@@ -101,6 +121,37 @@ class SquareDispCanvas:
                                                   (canvasWidth / 2) + (squareSize[0] / 2),
                                                   (canvasHeight / 2) + (squareSize[1] / 2), fill="#cce0ff",
                                                   width=0)
+
+    def canvasScrolling(self):  # TODO change scrolling to canvasZoom
+        self.scroll_x = Scrollbar(self.frame, orient="horizontal", command=self.canvas.xview)
+        self.scroll_x.grid(row=1, column=0, sticky=EW)
+        self.scroll_y = Scrollbar(self.frame, orient="vertical", command=self.canvas.yview)
+        self.scroll_y.grid(row=0, column=1, sticky=NS)
+        self.canvas.configure(yscrollcommand=self.scroll_y.set, xscrollcommand=self.scroll_x.set)
+        self.canvas.grid()
+
+    def canvasZoomButtons(self):  # TODO "fill" frame button
+        self.canvasZoomButtonFrame = Frame(self.frame)
+        self.canvasZoomButtonFrame.grid(column=2, row=0, sticky=N)
+
+        self.zoomInButton = Button(self.canvasZoomButtonFrame, text="+", width=3, command=self.zoomIn)
+        self.zoomOutButton = Button(self.canvasZoomButtonFrame, text="-", width=3, command=self.zoomOut)
+        self.fillButton = Button(self.canvasZoomButtonFrame, text="Fill", width=3)
+
+        self.zoomInButton.pack()
+        self.zoomOutButton.pack()
+        self.fillButton.pack()
+
+    def zoomIn(self):
+        self.canvas.zoom(1.1)
+
+    # TODO can't zoom out past maximum
+    def zoomOut(self):
+        self.canvas.zoom(.9)
+
+    def fit(self):
+
+
 
 
 
